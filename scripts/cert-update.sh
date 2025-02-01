832ca49ddd1744e550e00d6378656ddb90f8249e
kev-techi <kevin@techi.be>
2025-02-01 16:18:56 +0100

    updated script

--- scripts/cert-update.sh
+++ scripts/cert-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DOMAIN=$1
+EMAIL=$2
 
-cd $HOME/engine
-docker compose run --rm certbot-renew certonly --non-interactive --webroot -w /var/www/certbot -d $DOMAIN
+cd $HOME/loan-pool
+docker compose run --rm certbot-renew certonly --non-interactive --webroot -w /var/www/certbot --agree-tos --email $EMAIL -d $DOMAIN
 
 docker compose restart server
